28c6f1960ee0ebd6f4c99a708acad4b228045754
Admin <admin@mac-admin.local>
2018-06-10 15:09:16 +0300

    Initial Commit

---
+++ Driveo Driver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Driveo Driver
+//
+//  Created by Admin on 6/10/18.
+//  Copyright © 2018 Ibtkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Driveo DriverTests/Driveo_DriverTests.swift
@@ -0,0 +1,36 @@
+//
+//  Driveo_DriverTests.swift
+//  Driveo DriverTests
+//
+//  Created by Admin on 6/10/18.
+//  Copyright © 2018 Ibtkar. All rights reserved.
+//
+
+import XCTest
+@testable import Driveo_Driver
+
+class Driveo_DriverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
