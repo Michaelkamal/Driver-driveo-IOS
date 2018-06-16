779819ee39ee47d47282b85574bdf3ce9c567f89
3alilio <35105321+3alilio@users.noreply.github.com>
2018-06-16 17:38:35 +0300

    update orderDetails view

--- Driveo Driver/Screens/OrderDetails/OrderDetailesView.swift
+++ Driveo Driver/Screens/OrderDetails/OrderDetailesView.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class OrderDetailesView: UIViewController {
 
+    
+    
+    @IBOutlet weak var orderNumber: UILabel!
+
+    @IBOutlet weak var orderDate: UILabel!
+
+    @IBOutlet weak var orderTime: UILabel!
+    
+    @IBOutlet weak var orderId: UILabel!
+    
+    @IBOutlet weak var orderTitle: UILabel!
+    
+    
+    @IBOutlet weak var orderDescription: UILabel!
+    
+    
+    
+    @IBOutlet weak var ordersImageCollection: UICollectionView!
+    
+    @IBOutlet weak var orderPaymentMethod: UILabel!
+    
+    @IBOutlet weak var orderPickup: UILabel!
+    
+    @IBOutlet weak var orderDropOff: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
